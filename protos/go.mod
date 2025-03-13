64805a316a4df8320885e4322c50aec1e934bc56
Alex <38862440+amr0ny@users.noreply.github.com>
2025-03-13 14:20:34 +0300

    feat: protos updated

--- protos/go.mod
+++ protos/go.mod
@@ -1,1 +1,3 @@
-module protos
+module github.com/amr0ny/qoquiz/protos
+
+go 1.24.0
---
+++ quizservice/main.go
@@ -0,0 +1,55 @@
+package quizservice
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"quizservice/common"
+	"strings"
+)
+
+const defaultServicePort = "80001"
+const defaultPprofPort = "8081"
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
+
+func initPprof(pprofPort string) {
+	log, err := common.GetLogger()
+	if err != nil {
+		fmt.Println(err)
+	}
+	addr := fmt.Sprintf("localhost: %v", pprofPort)
+	go func() {
+		log.Infoln("Starting pprof on %v", addr)
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			log.Errorf("failed running pprof: %v", err)
+		}
+	}()
+}
+
+func main() {
+	err := common.InitLoggerConfig(&common.Config{Filepath: getEnv("LOG_PATH", defaultLogPath)})
+	if err != nil {
+		fmt.Println(err)
+	}
+	log, err := common.GetLogger()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if strings.ToLower(getEnv("DEBUG", "")) == "true" {
+		log.Level = logrus.DebugLevel
+	}
+
+	if strings.ToLower(getEnv("PROFILING_DISABLED", "FALSE")) == "false" {
+		pprofPort := getEnv("PPROF_PORT", defaultPprofPort)
+		initPprof(pprofPort)
+	}
+	port := getEnv("SERVICE_PORT", defaultServicePort)
+
+}
